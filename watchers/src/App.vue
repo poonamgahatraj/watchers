8d9b1c92c9a2800962ca7d4c9759f9605f94793d
Poonam Gahatraj <poonamgtrz@gmail.com>
2023-10-28 11:34:26 +0545

    watchers

---
+++ watchers/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import watchers from './pages/watchers.vue';
+</script>
+
+<template>
+  <watchers/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ watchers/src/pages/watchers.vue
@@ -0,0 +1,23 @@
+<template>
+    GB:<input type="number" v-model.number="gb">
+    MB:<input type="number" v-model.number="mb">
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            gb:null,
+            mb:null,
+         }
+    },
+    watch:{
+        gb(value){
+            this.mb=value*1024
+        },
+        mb(value){
+            this.gb=value/1024
+        }
+    }
+}
+</script>
