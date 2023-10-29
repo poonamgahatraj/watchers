b005caa341a556079180c8fd27b82aa4ae26df54
Poonam Gahatraj <poonamgtrz@gmail.com>
2023-10-29 14:56:28 +0545

    watchers

--- watchers/src/App.vue
+++ watchers/src/App.vue
@@ -2,10 +2,11 @@
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import watchers from './pages/watchers.vue';
+import createmethod from './components/createmethod.vue';
 </script>
 
 <template>
-  <watchers/>
+<createmethod/>
 </template>
 
 <style scoped>
---
+++ watchers/src/components/createmethod.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1>Hello</h1>
+    <h2>Good morning</h2>
+</template>
+
+<script>
+export default{
+
+
+    //beforeCreate(){
+    //    alert ("before create")
+    //},
+
+    //created(){
+     //   alert("component created")
+    //}
+
+    beforeMount(){
+        console.log("beforemount", this.$el)
+    },
+
+    mounted(){
+        console.log("mounted",this.$el)
+    }
+
+}
+</script>
+
+<style>
+h1{
+    color:red;
+}
+</style>
